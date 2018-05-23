3d4748fd4f68b870ec1b0437ee99c6542eaa478f
90miguelangel <90miguelangel@gmail.com>
2018-05-22 22:57:20 -0500

    added new test cases

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   
-it "get score from input" do
+it "get 300 when all frames are strikes" do
   game = Game.new "XXXXXXXXXXXX"
   score = game.get_score
   expect(score).to eq 300
+end
+
+it "get 90 when are 10 pairs of 9 and miss" do
+  game = Game.new "9-9-9-9-9-9-9-9-9-9-"
+  score = game.get_score
+  expect(score).to eq 90
+end
+
+it "get 90 when are 10 pairs of 5 and spare, with a final 5" do
+  game = Game.new "5/5/5/5/5/5/5/5/5/5/5"
+  score = game.get_score
+  expect(score).to eq 150
 end
 
 end
